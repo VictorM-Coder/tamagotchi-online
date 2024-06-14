088da8b9eeb067e714d1c309f70ed88f6f1c66cb
VictorM-Coder <martinsvictor6781@gmail.com>
2024-06-14 12:02:31 -0300

    style(client): improve chat message style

--- client/public/scripts/components/ChatActionComponent.js
+++ client/public/scripts/components/ChatActionComponent.js
@@ -6,7 +6,7 @@ class ChatActionComponent extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-            <div class="chat-message">[${this.chatAction.dateTime}]<strong>${this.chatAction.username}</strong>: ${this.chatAction.action}</div>
+            <div class="chat-message">[${this.getDateTimeFormated()}]<strong>${this.chatAction.username}</strong> ${this.getActionMessage()}</div>
         `
     }
 
@@ -17,6 +17,26 @@ class ChatActionComponent extends HTMLElement {
     set chatAction(value) {
         if (value) {
             this._chatAction = value
+        }
+    }
+
+    getDateTimeFormated() {
+        const date = new Date(this.chatAction.dateTime)
+        return date.toLocaleString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' })
+    }
+
+    getActionMessage() {
+        switch (this.chatAction.action) {
+            case 'EAT':
+                return 'fed the Tamagotchi.';
+            case 'SLEEP':
+                return 'put the Tamagotchi to sleep.';
+            case 'AWAKE':
+                return 'woke up the Tamagotchi.';
+            case 'PLAY':
+                return 'played with the Tamagotchi.';
+            case 'NAME':
+                return 'created the Tamagotchi.';
         }
     }
 }
